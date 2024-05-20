8e36364cbb7f36dc70ebd1befc3536a2ea8f70af
suraartut <suraartut@gmail.com>
2024-05-20 16:25:00 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import Game from "@/components/Game";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <Game/>
